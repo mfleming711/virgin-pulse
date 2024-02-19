f20780e68c3c906665cbfbb7406137fc31212ae6
mfleming711 <mfleming.duck@gmail.com>
2024-02-19 17:26:49 +0100

    added expanding animated card

--- templates/components/expanding-card-animated/expanding-card-animated.js
+++ templates/components/expanding-card-animated/expanding-card-animated.js
@@ -6,10 +6,17 @@ gsap.registerPlugin(ScrollTrigger);
 window.expandingCardAnimatedComponent = () => {
   return {
     init() {
+      const uniqueId = `clipPath${Math.random()
+        .toString(36)
+        .substr(2, 9)}`;
+      this.$el.querySelector('clipPath').id = uniqueId;
+      this.$el.querySelector(
+        '.component-expanding-card-animated__card'
+      ).style.clipPath = `url(#${uniqueId})`;
       gsap.to(this.$el, {
         scrollTrigger: {
           trigger: this.$el,
-          markers: true,
+          markers: false,
           start: 'top 50%',
           onEnter: () => this.$el.classList.add('expanded'),
         },
